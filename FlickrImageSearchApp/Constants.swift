b625df6ba08f0bfd3295caeaa625fa6543107d4b
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-05-05 17:24:07 +0300

    Change Constants

--- FlickrImageSearchApp/Constants.swift
+++ FlickrImageSearchApp/Constants.swift
@@ -9,9 +9,9 @@ import Foundation
 
 class Constants: NSObject {
 
-    static let api_key = "5b189ce4293089d8736f7bbf2882a960"
-    static let per_page = 60
-    static let searchURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(Constants.api_key)&format=json&nojsoncallback=1&safe_search=1&per_page=60&text=%@&page=%ld"
+    static let apikey = "5b189ce4293089d8736f7bbf2882a960"
+    static let perPage = 60
+    static let baseURL = "https://api.flickr.com"
     static let imageURL = "http://farm%d.static.flickr.com/%@/%@_%@.jpg"
         
     static let defaultColumnCount: Int = 2
