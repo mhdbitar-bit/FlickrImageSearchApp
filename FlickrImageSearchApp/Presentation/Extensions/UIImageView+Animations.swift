a6e77ad0e6b30b13d48451bfd3cdedcc5a24f4da
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-05-05 21:46:00 +0300

    Add shimmering effect

---
+++ FlickrImageSearchApp/Presentation/Extensions/UIImageView+Animations.swift
@@ -0,0 +1,21 @@
+//
+//  UIImageView+Animations.swift
+//  FlickrImageSearchApp
+//
+//  Created by Mohammad Bitar on 5/5/22.
+//
+
+import UIKit
+
+extension UIImageView {
+    func setImageAnimated(_ newImage: UIImage?) {
+        image = newImage
+        
+        guard newImage != nil else { return }
+        
+        alpha = 0
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+}
