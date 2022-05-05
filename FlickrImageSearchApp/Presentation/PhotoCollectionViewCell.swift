118b4124572e20390a6b307ce09a08fec4351051
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-05-05 17:04:01 +0300

    add `PhotoCollectionViewCell`

---
+++ FlickrImageSearchApp/Presentation/PhotoCollectionViewCell.swift
@@ -0,0 +1,56 @@
+//
+//  PhotoCollectionViewCell.swift
+//  FlickrImageSearchApp
+//
+//  Created by Mohammad Bitar on 5/5/22.
+//
+
+import UIKit
+import Combine
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var photo: UIImageView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
+    private var viewModel: PhotoCollectionViewCellViewModel!
+    private var cancellables: Set<AnyCancellable> = []
+    
+    convenience init(viewModel: PhotoCollectionViewCellViewModel) {
+        self.init()
+        self.viewModel = viewModel
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        bind()
+    }
+    
+    private func bind() {
+        bindLoading()
+        bindData()
+    }
+    
+    private func bindLoading() {
+        viewModel.$isLoading.sink { [weak self] isLoading in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                if isLoading {
+                    self.activityIndicator.startAnimating()
+                } else {
+                    self.activityIndicator.stopAnimating()
+                }
+            }
+        }.store(in: &cancellables)
+    }
+    
+    private func bindData() {
+        viewModel.$data.sink { [weak self] data in
+            guard let self = self else { return }
+            if let data = data {
+                self.photo.image = UIImage(data: data)
+            }
+        }.store(in: &cancellables)
+    }
+}
