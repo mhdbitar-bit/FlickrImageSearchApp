648c1b88e7400539bf40f3e4772e877c33413cf1
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-05-05 17:24:46 +0300

    add loadPhotos method in `FlickrCollectionViewControllerViewModel`

--- FlickrImageSearchApp/Presentation/ViewModels/FlickrCollectionViewControllerViewModel.swift
+++ FlickrImageSearchApp/Presentation/ViewModels/FlickrCollectionViewControllerViewModel.swift
@@ -6,15 +6,38 @@
 //
 
 import Foundation
+import Combine
 
 final class FlickrCollectionViewControllerViewModel {
-    private var remoteService: RemotePhotoService
-    private var imageService: ImageDataService
+    var remoteService: RemotePhotoService
+    var imageService: ImageDataService
+    
+    let title = "Photos"
+    @Published var photos: [Photo] = []
+    @Published var isLoading: Bool = false
+    @Published var error: String? = nil
     
     init(remoteService: RemotePhotoService, imageService: ImageDataService) {
         self.remoteService = remoteService
         self.imageService = imageService
     }
     
+    func loadPhotos(keyword: String) {
+        isLoading = true
+        if let url = URL(string: Constants.baseURL) {
+            let endpoint = FlickrEndpoint.getPhotos.url(baseURL: url, keyword: keyword, perPage: Constants.perPage, page: 1)
+            remoteService.getPhotos(url: endpoint, completion: handleAPIResult)
+        }
+    }
     
+    private func handleAPIResult(_ result: Result<[Photo], Error>) {
+        isLoading = false
+        switch result {
+        case let .success(photos):
+            self.photos = photos
+            
+        case let .failure(error):
+            self.error = error.localizedDescription
+        }
+    }
 }
