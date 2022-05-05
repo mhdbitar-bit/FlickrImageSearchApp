8c4d6c1c5ec6dedcd51b51d8a5341eb04f5a05dd
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-05-05 16:07:04 +0300

    Add `FlickrEndpoint`

---
+++ FlickrImageSearchApp/Photo API/FlickrEndpoint.swift
@@ -0,0 +1,33 @@
+//
+//  FlickrEndpoint.swift
+//  FlickrImageSearchApp
+//
+//  Created by Mohammad Bitar on 5/5/22.
+//
+
+import Foundation
+
+enum FlickrEndpoint {
+    case getPhotos
+    
+    func url(baseURL: URL, keyword: String, perPage: Int, page: Int) -> URL {
+        switch self {
+        case .getPhotos:
+            var components = URLComponents()
+            components.scheme = baseURL.scheme
+            components.host = baseURL.host
+            components.path = baseURL.path + "/services/rest"
+            components.queryItems = [
+                URLQueryItem(name: "method", value: "flickr.photos.search"),
+                URLQueryItem(name: "api_key", value: Constants.api_key),
+                URLQueryItem(name: "format", value: "json"),
+                URLQueryItem(name: "nojsoncallback", value: "1"),
+                URLQueryItem(name: "safe_search", value: "1"),
+                URLQueryItem(name: "per_page", value: "\(perPage)"),
+                URLQueryItem(name: "text", value: keyword),
+                URLQueryItem(name: "page", value: "\(page)")
+            ]
+            return components.url!
+        }
+    }
+}
