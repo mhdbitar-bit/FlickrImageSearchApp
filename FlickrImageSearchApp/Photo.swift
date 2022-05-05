4ef4d8eb6d2d074a5e5ac12ef64f4de3b76b6a9d
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-05-05 15:30:15 +0300

    Change the attributes of Photo model

--- FlickrImageSearchApp/Photo.swift
+++ FlickrImageSearchApp/Photo.swift
@@ -8,13 +8,5 @@
 import Foundation
 
 struct Photo: Equatable {
-    let id : String
-    let owner: String
-    let secret : String
-    let server : String
-    let farm : Int
-    let title: String
-    let ispublic : Int
-    let isfriend : Int
-    let isfamily : Int
+    let url: URL
 }
