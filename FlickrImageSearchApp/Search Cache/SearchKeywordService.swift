c08d1fd239f29fec5f5488eba911beb2d0682b57
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-05-06 19:50:01 +0300

    Implement `SearchKeywordService`

---
+++ FlickrImageSearchApp/Search Cache/SearchKeywordService.swift
@@ -0,0 +1,40 @@
+//
+//  SearchKeywordService.swift
+//  FlickrImageSearchApp
+//
+//  Created by Mohammad Bitar on 5/6/22.
+//
+
+import Foundation
+
+final class SearchKeywordService {
+    private let store: SearchKeywoardStore
+    
+    init(store: SearchKeywoardStore) {
+        self.store = store
+    }
+}
+
+extension SearchKeywordService: SearchKeywoardStore {
+    func insert(_ keyword: String, completion: @escaping (InsertionResult) -> Void) {
+        store.insert(keyword) { result in
+            switch result {
+            case .failure(let error):
+                completion(.failure(error))
+            default: break
+            }
+        }
+    }
+    
+    func retrieve(completion: @escaping (RetrievalResult) -> Void) {
+        store.retrieve { result in
+            switch result {
+            case .success(let keywords):
+                completion(.success(keywords))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
+
