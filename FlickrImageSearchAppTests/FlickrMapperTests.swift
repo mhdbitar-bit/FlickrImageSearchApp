17638548036b8cac3e6e85a77183994b24b17e1a
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-05-07 21:51:07 +0300

    test test_map_throwsErrorOn200HTTPResponseWithInvalidJSON

--- FlickrImageSearchAppTests/FlickrMapperTests.swift
+++ FlickrImageSearchAppTests/FlickrMapperTests.swift
@@ -14,6 +14,14 @@ final class FlickrMapperTests: XCTestCase {
         }
     }
     
+    func test_map_throwsErrorOn200HTTPResponseWithInvalidJSON() throws {
+        let invalidJSON = Data("invalid json".utf8)
+        
+        XCTAssertThrowsError(
+            try FlickrMapper.map(invalidJSON, from: HTTPURLResponse(statusCode: 200))
+        )
+    }
+    
     private func makeJson(_ photos: [String: Any]) -> Data {
         let json = ["photos": photos]
         return try! JSONSerialization.data(withJSONObject: json)
