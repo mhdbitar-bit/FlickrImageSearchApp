dd17c2c703ed27c6133626107c6eb5a6eadd5ace
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-05-07 21:26:57 +0300

    Add test for search photos endpoint

---
+++ FlickrImageSearchAppTests/FlickrEndpointTests.swift
@@ -0,0 +1,19 @@
+@testable import FlickrImageSearchApp
+import XCTest
+
+final class FlickrEndpointTests: XCTestCase {
+    
+    func test_searchPhotos_endpointURL() throws {
+        let baseURL = URL(string: "https://any-url.com")!
+        let key = Constants.apikey
+        let searchTerm = "any"
+        let perPage = Constants.perPage
+        let page = 1
+        let received = FlickrEndpoint.searchPhotos.url(baseURL: baseURL, keyword: searchTerm, perPage: perPage, page: page)
+        
+        XCTAssertEqual(received.scheme, "https", "scheme")
+        XCTAssertEqual(received.host, "any-url.com", "host")
+        XCTAssertEqual(received.path, "/services/rest", "path")
+        XCTAssertEqual(received.query, "method=flickr.photos.search&api_key=\(key)&format=json&nojsoncallback=1&safe_search=1&per_page=\(perPage)&text=\(searchTerm)&page=\(page)", "query")
+    }
+}
