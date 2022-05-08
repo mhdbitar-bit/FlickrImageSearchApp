0e1c1f1dd4181752f94e513c244125baa841c9e8
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-05-08 12:04:30 +0300

    Refactor the retrieving test

--- FlickrImageSearchAppTests/CoreDataImageSearchStoreTests.swift
+++ FlickrImageSearchAppTests/CoreDataImageSearchStoreTests.swift
@@ -20,17 +20,10 @@ final class CoreDataImageSearchStoreTests: XCTestCase {
         let sut = makeSUT()
         let term = "any"
         
-        sut.insert(term) { result in
+        sut.insert(term) { [weak self] result in
             switch result {
             case .success:
-                sut.retrieve { retrieveResult in
-                    switch retrieveResult {
-                    case .success(let values):
-                        XCTAssertEqual(values.count, 1)
-                    case .failure(let error):
-                        XCTFail("Expected to retrieve empty terms, got \(error) instead")
-                    }
-                }
+                self?.expectToRetrieve(sut, expectedResult: [term])
             case .failure(let error):
                 XCTFail("Expected to insert term, got \(error) instead")
             }
@@ -40,7 +33,7 @@ final class CoreDataImageSearchStoreTests: XCTestCase {
 //    func test_retrieve_deliversValuesOnNonEmptyCache() {
 //        let sut = makeSUT()
 //        let terms = ["search tearm 1", "search term 2"]
-//        
+//
 //        sut.insert(<#T##keyword: String##String#>, completion: <#T##(Result<Void, Error>) -> Void#>)
 //    }
     
@@ -50,5 +43,16 @@ final class CoreDataImageSearchStoreTests: XCTestCase {
         let storeURL = URL(fileURLWithPath: "/dev/null")
         let sut = try! CoreDataSearchStore(storeURL: storeURL)
         return sut
+    }
+    
+    private func expectToRetrieve(_ sut: SearchKeywoardStore, expectedResult: [String], file: StaticString = #filePath, line: UInt = #line) {
+        sut.retrieve { retrieveResult in
+            switch retrieveResult {
+            case .success(let values):
+                XCTAssertEqual(values.count, expectedResult.count)
+            case .failure(let error):
+                XCTFail("Expected to retrieve empty terms, got \(error) instead", file: file, line: line)
+            }
+        }
     }
 }
