93f151a0431fd53b00ef280ddec08562a4db6845
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-05-08 12:00:43 +0300

    test inserting search term in core data

--- FlickrImageSearchAppTests/CoreDataImageSearchStoreTests.swift
+++ FlickrImageSearchAppTests/CoreDataImageSearchStoreTests.swift
@@ -16,6 +16,34 @@ final class CoreDataImageSearchStoreTests: XCTestCase {
         }
     }
     
+    func test_insert_searchTerm() {
+        let sut = makeSUT()
+        let term = "any"
+        
+        sut.insert(term) { result in
+            switch result {
+            case .success:
+                sut.retrieve { retrieveResult in
+                    switch retrieveResult {
+                    case .success(let values):
+                        XCTAssertEqual(values.count, 1)
+                    case .failure(let error):
+                        XCTFail("Expected to retrieve empty terms, got \(error) instead")
+                    }
+                }
+            case .failure(let error):
+                XCTFail("Expected to insert term, got \(error) instead")
+            }
+        }
+    }
+    
+//    func test_retrieve_deliversValuesOnNonEmptyCache() {
+//        let sut = makeSUT()
+//        let terms = ["search tearm 1", "search term 2"]
+//        
+//        sut.insert(<#T##keyword: String##String#>, completion: <#T##(Result<Void, Error>) -> Void#>)
+//    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> SearchKeywoardStore {
